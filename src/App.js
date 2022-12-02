dd8823ff7ad5a821b1050448e96dae0a9f4c1a33
Dylan McAulay <dmcaulay97@gmail.com>
2022-12-02 13:19:36 -0500

    all word checking functionality completed

--- src/App.js
+++ src/App.js
@@ -6,89 +6,185 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
-  const startingBoard = []
+	const startingBoard = []
 
-  for (let i = 0; i < 6; i++) {
-    const row = []
-    for (let j = 0; j < 5; j++) {
-      row.push(
-        {
-          letter: "",
-          color: "#000000",
-        }
-      )
-    }
-    startingBoard.push(row)
-  }
+	for (let i = 0; i < 6; i++) {
+		const row = []
+		for (let j = 0; j < 5; j++) {
+			row.push(
+				{
+					letter: "",
+					color: "#000000",
+				}
+			)
+		}
+		startingBoard.push(row)
+	}
 
-  // const startingBoard = [
-  //   ["", "", "", "", ""],
-  //   ["", "", "", "", ""],
-  //   ["", "", "", "", ""],
-  //   ["", "", "", "", ""],
-  //   ["", "", "", "", ""],
-  //   ["", "", "", "", ""]
-  // ]
+	const [boardState, setBoardState] = useState(startingBoard)
 
-  const [boardState, setBoardState] = useState(startingBoard)
+	const [currentBox, setCuttentBox] = useState(0)
 
-  const [currentBox, setCuttentBox] = useState(0)
+	const [currentBeginning, setCurrentBeginning] = useState(0)
 
-  const [currentBeginning, setCurrentBeginning] = useState(0)
+	const [word, setWord] = useState("TITII")
 
-  const [word, setWord] = useState("FARTS")
+	const [disableKeyboard, setDisableKeyboard] = useState(false)
 
-  const type = (letter) => {
-    const currentEnding = currentBeginning + 5
-    if (currentBox < currentEnding) {
-      let newBoardState = [...boardState];
-      const row = Math.floor(currentBox / 5)
-      const col = currentBox % 5
-      newBoardState[row][col].letter = letter
-      setBoardState(newBoardState)
-      setCuttentBox(currentBox + 1)
-    }
-  }
+	const type = (letter) => {
+		if (!disableKeyboard) {
+			const currentEnding = currentBeginning + 5
+			if (currentBox < currentEnding) {
+				let newBoardState = [...boardState];
+				const row = Math.floor(currentBox / 5)
+				const col = currentBox % 5
+				newBoardState[row][col].letter = letter
+				setBoardState(newBoardState)
+				setCuttentBox(currentBox + 1)
+			}
+		}
+	}
 
-  const backspace = () => {
-    if (currentBox > currentBeginning) {
-      let newBoardState = [...boardState];
-      const row = Math.floor((currentBox - 1) / 5)
-      const col = (currentBox - 1) % 5
-      newBoardState[row][col].letter = ""
-      setBoardState(newBoardState)
-      setCuttentBox(currentBox - 1)
-    }
-  }
+	const backspace = () => {
+		if (!disableKeyboard) {
+			if (currentBox > currentBeginning) {
+				let newBoardState = [...boardState];
+				const row = Math.floor((currentBox - 1) / 5)
+				const col = (currentBox - 1) % 5
+				newBoardState[row][col].letter = ""
+				setBoardState(newBoardState)
+				setCuttentBox(currentBox - 1)
+			}
+		}
+	}
 
-  const checkWord = () => {
-    const currentEnding = currentBeginning + 5
-    if (currentBox === currentEnding) {
-      const row = Math.floor(currentBox / 5) - 1;
-      for (let i = 0; i < 5; i++) {
-        const guessLetter = boardState[row][i].letter
-        const newBoardState = [...boardState]
-        if (guessLetter === word[i]) {
-          newBoardState[row][i].color = "#538d4e"
-          setBoardState(newBoardState)
-        } else if (word.includes(guessLetter)) {
-          newBoardState[row][i].color = "#b59f3b"
-          setBoardState(newBoardState)
-        }
-      }
-      setCurrentBeginning(currentBeginning + 5);
-    }
-  }
+	const doubleCheck = (word) => {
+		let doubles = []
 
-  return (
-    <div className="App">
-      <Header />
-      <div className='game-area'>
-        <Board boardState={boardState} />
-        <Keyboard type={type} backspace={backspace} checkWord={checkWord} />
-      </div>
-    </div>
-  );
+		for (let i = 0; i < word.length; i++) {
+			for (let j = i + 1; j < word.length; j++) {
+				if (word[i] === word[j]) {
+					if (!doubles.includes(word[i])) {
+						doubles.push(word[i])
+					}
+				}
+			}
+		}
+
+		doubles = doubles.map((letter) => {
+			return { letter: letter, count: 0 }
+		})
+
+		doubles.forEach((letterObj) => {
+			for (let i = 0; i < word.length; i++) {
+				if (word[i] === letterObj.letter) {
+					letterObj.count += 1
+				}
+			}
+		})
+
+		return doubles
+	}
+
+
+	const checkWord = () => {
+		const doubles = doubleCheck(word)
+		const currentEnding = currentBeginning + 5
+
+
+		if (currentBox === currentEnding) {
+
+			const row = Math.floor(currentBox / 5) - 1;
+			const exclude = []
+
+			for (let i = 0; i < 5; i++) {
+				const guessLetter = boardState[row][i].letter
+				const newBoardState = [...boardState]
+
+				let isDouble = false
+				let doubleArrayIndex = 0
+				doubles.forEach((letterObj, index) => {
+					if (guessLetter === letterObj.letter) {
+						isDouble = true
+						doubleArrayIndex = index
+					}
+				})
+
+				if (guessLetter === word[i]) {
+					exclude.push(i)
+					if (isDouble) {
+						if (doubles[doubleArrayIndex].count > 0) {
+							newBoardState[row][i].color = "#538d4e"
+							setBoardState(newBoardState)
+							doubles[doubleArrayIndex].count -= 1
+						} else {
+							newBoardState[row][i].color = "#3a3a3c"
+							setBoardState(newBoardState)
+						}
+					} else {
+						newBoardState[row][i].color = "#538d4e"
+						setBoardState(newBoardState)
+					}
+				}
+
+			}
+			for (let i = 0; i < 5; i++) {
+				const guessLetter = boardState[row][i].letter
+				const newBoardState = [...boardState]
+
+				let isDouble = false
+				let doubleArrayIndex = 0
+				doubles.forEach((letterObj, index) => {
+					if (guessLetter === letterObj.letter) {
+						isDouble = true
+						doubleArrayIndex = index
+					}
+				})
+
+				if (!exclude.includes(i)) {
+					if (word.includes(guessLetter)) {
+						if (isDouble) {
+							if (doubles[doubleArrayIndex].count > 0) {
+								newBoardState[row][i].color = "#b59f3b"
+								setBoardState(newBoardState)
+								doubles[doubleArrayIndex].count -= 1
+							} else {
+								newBoardState[row][i].color = "#3a3a3c"
+								setBoardState(newBoardState)
+							}
+						} else {
+							newBoardState[row][i].color = "#b59f3b"
+							setBoardState(newBoardState)
+						}
+					} else {
+						newBoardState[row][i].color = "#3a3a3c"
+						setBoardState(newBoardState)
+					}
+				}
+
+			}
+			if (exclude.length === 5) {
+				console.log("You Win")
+				setDisableKeyboard(true)
+			} else if (currentBeginning === 25) {
+				console.log("Better Luck Next Time")
+				setDisableKeyboard(true)
+			} else {
+				setCurrentBeginning(currentBeginning + 5)
+			}
+
+		}
+	}
+
+	return (
+		<div className="App">
+			<Header />
+			<div className='game-area'>
+				<Board boardState={boardState} />
+				<Keyboard type={type} backspace={backspace} checkWord={checkWord} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
